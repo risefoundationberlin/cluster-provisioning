363d3540149cccff1120ddcc81ddaba76dd670c1
Piyush Sardana <piyush.sardana@risefoundation.de>
2022-03-07 14:37:25 +0100

    removed output variable

--- output.tf
+++ output.tf
@@ -1,15 +1,3 @@
-output "resource_group_name" {
-    value = azurerm_resource_group.state-rg.name
-}
 output "kubernetes_cluster_name" {
     value = azurerm_kubernetes_cluster.rf-prod.name
-}
-output "terraform_state_resource_group_name" {
-    value = azurerm_resource_group.state-rg.name
-}
-output "terraform_state_storage_account" {
-    value = azurerm_storage_account.state-sta.name
-}
-output "terraform_state_storage_container_core" {
-    value = azurerm_storage_container.core-container.name
 }
