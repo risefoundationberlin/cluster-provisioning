54bf388c5bae4374762116bf64971dce9c61da4d
Piyush Sardana <piyush.sardana@risefoundation.de>
2022-03-07 14:36:48 +0100

    removed resource group and storage container

--- cluster.tf
+++ cluster.tf
@@ -2,43 +2,6 @@ provider "azurerm" {
     features {}
 }
 
-resource "azurerm_resource_group" "state-rg" {
-    name     = "dev-ops-rg"
-    location = "Germany West Central"
-    tags = {
-        environment = "Production"
-    }
-    lifecycle {
-        prevent_destroy = true
-    }
-}
-
-# Create a Storage Account for the Terraform State File
-resource "azurerm_storage_account" "state-sta" {
-    depends_on = [azurerm_resource_group.state-rg]
-    name = "rfprodtfsa"
-    resource_group_name = "dev-ops-rg"
-    location = azurerm_resource_group.state-rg.location
-    account_kind = "StorageV2"
-    account_tier = "Standard"
-    access_tier = "Hot"
-    account_replication_type = "ZRS"
-    enable_https_traffic_only = true
-    lifecycle {
-        prevent_destroy = true
-    }
-    tags = {
-        environment = "Production"
-    }
-}
-
-# Create a Storage Container for the Core State File
-resource "azurerm_storage_container" "core-container" {
-    depends_on = [azurerm_storage_account.state-sta]
-    name = "core-tfstate"
-    storage_account_name = azurerm_storage_account.state-sta.name
-}
-
 resource "azurerm_kubernetes_cluster" "rf-prod" {
     name = "rf-prod"
     location = azurerm_resource_group.state-rg.location
